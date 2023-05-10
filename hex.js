91d9c1eae5dae56b3fac901d2939ed68fda9de55
Braden Gerace <braden.gerace@hotmail.com>
2023-05-10 17:38:56 -0400

    Update hex.js

--- hex.js
+++ hex.js
@@ -3,10 +3,15 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
-    let hexColor = "#";
+    let hexColor = "#";   
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[0];
+        let hexNumber = getRandomNumber();
+        hexColor += hex[hexNumber];
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 })
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
